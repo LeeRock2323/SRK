74c5aeb4cdc9268283d9c936dcc27f491601bded
LeeRock2323 <leerock.zero@gmail.com>
2020-03-22 11:41:04 +0100

    change

---
+++ js/macy.js
@@ -0,0 +1,22 @@
+var macy = Macy({
+    container: "#macy1",
+    trueOrder: false,
+    waitForImages: false,
+    margin: 64,
+    columns: 3,
+    breakAt: {
+        1500: 2,
+        1000: 1
+    }
+});
+var macy2 = Macy({
+    container: "#macy2",
+    trueOrder: false,
+    waitForImages: false,
+    margin: 64,
+    columns: 3,
+    breakAt: {
+        1500: 2,
+        1000: 1
+    }
+});
